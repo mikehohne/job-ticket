0692b93bdcbe2ddbfee52225a84c05b763d99a0e
Vince Scheuerman <vsherms@gmail.com>
2017-08-22 18:57:06 -0700

    Initial commit, added Product Model

---
+++ job-ticket/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace job_ticket.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public int productID { get; set; }
+		public string productName { get; set; }
+		public string imageURL { get; set; }
+		public bool hasTemplate { get; set; }
+		public float stockQuantity { get; set; }
+
+	}
+}
---
+++ job-ticket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(job_ticket.Startup))]
+namespace job_ticket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
